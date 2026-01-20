0a8376eb51c4639cd79592802abd7d48bdc70ffa
Bridget <bridgetnamugga@gmail.com>
2026-01-20 11:08:18 +0300

    layOut:Added app layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 
-function App() {
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
+import LayOut from "./layOut";
 
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<LayOut/>,
+    errorElement:<p>Hello</p>,
+    children: [
+   
+    ]
+  }
+  
+])
+function App() {
+  
   return (
-    <>
-  
-  
-    </>
+      <div id='light'>
+        <RouterProvider router={router}/>
+      </div>
   )
 }
 
