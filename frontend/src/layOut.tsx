7bf8f387dc9a62fcd1ad38023696a284326ea279
Bridget <bridgetnamugga@gmail.com>
2026-01-22 16:00:12 +0300

    LayOut:Added footer to the app layout

--- frontend/src/layOut.tsx
+++ frontend/src/layOut.tsx
@@ -1,11 +1,13 @@
 import {Outlet} from 'react-router-dom'
 import Nav from './pages/NavBar/nav'
+import Footer from './pages/Footer/footer'
 
 function LayOut() {
     return (
         <>
         <Nav/>
         <Outlet/>
+        <Footer/>
         </>
     )
 }
