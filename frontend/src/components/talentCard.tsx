bde80903b5e142754c29932f8b0386d7a625a4b8
Bridget <bridgetnamugga@gmail.com>
2026-01-22 15:46:28 +0300

    Feat:Added hero section

---
+++ frontend/src/components/talentCard.tsx
@@ -0,0 +1,55 @@
+interface Talent {
+    image:string,
+    title:string,
+    level:string,
+    name:string,
+    salary:string
+}
+
+
+const talents:Talent[] = [
+    {
+        image:'src/assets/talent.jpg',
+        title:'Software Developer',
+        level:'Senior',
+        name:'Bizzy',
+        salary:'$5000/month'
+    },
+       {
+        image:'src/assets/talent.jpg',
+        title:'Software Developer',
+        level:'Senior',
+        name:'Bizzy',
+        salary:'$5000/month'
+    },
+       {
+        image:'src/assets/talent.jpg',
+        title:'Software Developer',
+        level:'Senior',
+        name:'Bizzy',
+        salary:'$5000/month'
+    },
+       {
+        image:'src/assets/talent.jpg',
+        title:'Software Developer',
+        level:'Senior',
+        name:'Bizzy',
+        salary:'$5000/month'
+    }
+]
+
+
+export default function TalentSection() {
+
+    return(
+        <section>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+
+        </section>
+    )
+    
+}
