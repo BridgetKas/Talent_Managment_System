8ce896b93c944fc008024629a70dbb7556136706
Bridget <bridgetnamugga@gmail.com>
2026-01-23 16:36:22 +0300

    Feat:Added FAQ section to the home page

--- frontend/src/pages/Home/homePage.tsx
+++ frontend/src/pages/Home/homePage.tsx
@@ -1,9 +1,13 @@
+import FAQSection from "../../components/faq";
 import Hero from "../../components/hero";
+import TalentSection from "../../components/talentCard";
 
 export default function HomePage() {
     return (
-        <div>
+        <div className="flex flex-col">
             <Hero/>
+            <TalentSection/>
+            <FAQSection/>
 
         </div>
 
