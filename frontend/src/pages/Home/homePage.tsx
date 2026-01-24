4c41a493adc04414ba6e40570c5b5d93dfb27250
Bridget <bridgetnamugga@gmail.com>
2026-01-24 16:11:27 +0300

    layOut:Added why choose us section to the home page

--- frontend/src/pages/Home/homePage.tsx
+++ frontend/src/pages/Home/homePage.tsx
@@ -1,12 +1,16 @@
 import FAQSection from "../../components/faq";
 import Hero from "../../components/hero";
 import TalentSection from "../../components/talentCard";
+import Testimonial from "../../components/testimonial";
+import WhyChooseUs from "../../components/why-choose-us";
 
 export default function HomePage() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-4">
             <Hero/>
             <TalentSection/>
+            <WhyChooseUs/>
+            <Testimonial/>
             <FAQSection/>
 
         </div>
